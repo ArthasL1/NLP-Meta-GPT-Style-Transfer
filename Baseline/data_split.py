00edcae79358c793b58595233599c96e865df245
LIAN00001 <158861418+LIAN00001@users.noreply.github.com>
2024-03-25 22:18:14 +0800

    Add files via upload

--- Baseline/data_split.py
+++ Baseline/data_split.py
@@ -6,18 +6,16 @@ data_types = ['train', 'val', 'test']
 
 dfs = {data_type: pd.DataFrame() for data_type in data_types}
 
-
 for subdir, dirs, files in os.walk(data_folder):
     for file in files:
-
         for data_type in data_types:
-            if file.startswith(data_type) and file.endswith('.csv'):
+
+            if file.startswith(data_type) and file.endswith('.tsv'):
                 file_path = os.path.join(subdir, file)
 
-                df = pd.read_csv(file_path)
+                df = pd.read_csv(file_path, sep='\t')
                 dfs[data_type] = pd.concat([dfs[data_type], df], ignore_index=True)
 
-# 保存合并后的数据到新的CSV文件中
 for data_type, df in dfs.items():
     output_path = os.path.join(data_folder, f'{data_type}_combined.csv')
     df.to_csv(output_path, index=False)
