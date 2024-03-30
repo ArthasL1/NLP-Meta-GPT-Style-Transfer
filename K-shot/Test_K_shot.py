4fe7c00a43ede566b5b33cee6e2eea18529209a9
LIAN00001 <lianxubruce@163.com>
2024-03-31 06:17:05 +0800

    Update Test_K_shot.py

--- K-shot/Test_K_shot.py
+++ K-shot/Test_K_shot.py
@@ -199,9 +199,6 @@ def k_shot_evaluation(model, k_suppot_path, n_query_path,loss_dir,support_batch=
                     ret = model.forward(n_inputs, attention_mask=n_masks, labels=n_label)
                     query_loss += ret[0].item()
                     
-                    
-                    
-
                     # get actual text and predicted text
                     y_text, pred_text = y_pred_text(ret, n_inputs, n_label, gpt_tokenizer)
                     del ret
